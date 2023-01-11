359c0dd1076fa6b52034345b5b17b2f6a0e9b27b
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-01-11 22:13:09 +0100

    samples

--- app.py
+++ app.py
@@ -1,42 +1,96 @@
-foundNummber = 0
-name = "Oleh"
+# Variables
+print("-" * 10)
+print("Variables")
+NUMBER = 1
+MY_STRING = "my string "
+MY_LIST_NUMBERS = [0, 1, 2, 3,]
 
-for x in range(1, 10):
-    # print(f"{x % 2} number {x}")
-    if (x % 2) == 0:
-        print(x)
-        foundNummber = foundNummber + 1
-print(f"We have {foundNummber} even numbers")
+# Boxing/Unboxing
+print("-" * 10)
+print("Boxing/Unboxing")
+NUMBER_AS_STRING = str(NUMBER)
 
-# Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+# String Operations
+print("-" * 10)
+print("String Operations")
+MY_STRING = f"my string with \"Escape"
+MY_STRING = f"my string with \nwith new line"
+MY_STRING = f"my string with number {NUMBER}"
+
+print(MY_STRING[0: 10])
+print(len(MY_STRING))
+print(MY_STRING.upper())
+print(MY_STRING.lower())
+print(MY_STRING.title())
+print(
+    f"rstrip remove white spaces at the end of the string: {MY_STRING.rstrip()}")
+print(MY_STRING.replace("s", "S"))
+print("my" in MY_STRING)
+print("my" not in MY_STRING)
+
+# Number Operations
+print("-" * 10)
+print("Number Operations")
+# import math
+# print(10 ** 3) - multiple X time on itself. 10 * 10 * 10
+print(10 ** 3)
+print(10 % 3)
+print(10 // 3)
+
+# Control Flow
+print("-" * 10)
+print("Control Flow")
+
+# The closer letter to the end of alphabet then it's higher. M is higher than A
+print("apple" > "microsoft")
+
+# if
+if (15 > 30):
+    print("30")
+elif (30 == 30):
+    print(30)
+else:
+    print(0)
+
+# Ternary
+MY_STRING = "Hello" if 15 > 30 else "CHAO"
+
+# Logical
+HIGH_INCOME = True
+GOOD_CREDIT = True
+STUDENT = True
+
+if not STUDENT:
+    print("Can have credit")
+else:
+    print("NOT CREDIT FOR STUDENTS")
+
+if (HIGH_INCOME or GOOD_CREDIT) and not STUDENT:
+    print("Can have credit")
+else:
+    print("NOT CREDIT FOR STUDENTS")
+
+if HIGH_INCOME or GOOD_CREDIT and STUDENT:
+    print("Can have credit")
+else:
+    print("NOT CREDIT FOR STUDENTS")
+
+AGE = 19
+
+if 18 <= AGE < 65:
+    print("Eligible")
 
 
-def greet(name, owner):
-    if (name.lower() == owner.lower()):
-        return 'Hello boss'
-    else:
-        return 'Hello guest'
+for number in MY_LIST_NUMBERS:
+    print(f"Loop number: {number}")
+    if number == 0:
+        break
 
-# Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
-#   Return your answer as a number.
-
-
-def sum_mix(arr):
-    result = 0
-    for number in arr:
-        result = result + int(number)
-    return result
+# def multiply(*numbers):
+#     result = 1
+#     for number in numbers:
+#         result = result * number
+#     return 1
 
 
-print(sum_mix([9, 3, '7', '3']))
-
-#The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
-
-def century(year):
-    centruy = int(str(year)[0:1]) if year < 1000 else int(str(year)[0:2])
-    lastNumber = year % 10
-    nummberBeforeLast = int(str(year))
-
-    return centruy if (year % 10) == 0 else centruy + 1
-
-print(f"Year: {century(2023)}")
+# print(multiply(1, 2, 3, 4))
