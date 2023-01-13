7789736c9e010a9b994214ac8d292b1f25099535
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-01-13 22:11:59 +0100

    end list lesson

---
+++ data-struct.py
@@ -0,0 +1,14 @@
+# List
+
+nyStringsList = ["K", "E", "R", "A", "S"]
+myNumbersList = [0] * 100
+print(nyStringsList + myNumbersList)
+
+myNumberRange = range(20)
+myListNumber = list(myNumberRange)
+print(myListNumber)
+
+
+phraseList = list("Hello World")
+print(f"Phrase to list: {phraseList}")
+print(f"List length: {len(phraseList)}")
