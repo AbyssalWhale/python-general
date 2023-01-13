60a7a1ab49998bbfe0a029a65ec82d7765c1afbb
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-01-13 22:22:02 +0100

    acessing items

--- data-struct.py
+++ data-struct.py
@@ -12,3 +12,12 @@ print(myListNumber)
 phraseList = list("Hello World")
 print(f"Phrase to list: {phraseList}")
 print(f"List length: {len(phraseList)}")
+
+# Accessing Items
+print("-Accessing Items-")
+myLetters = ["a", "b", "c", "d"]
+print(f"zero item{myLetters[0]}")
+print(f"zero and next item{myLetters[0:1]}")
+print(f"next item{myLetters[:1]}")
+print(f"{list(range(20))[::2]}")
+print(f"\nreverse order{list(range(20))[::-1]}")
