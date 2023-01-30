26daeea9defeae24770f5554403a5bc3a4be4bdd
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-01-30 21:50:55 +0100

    dic

---
+++ .vs/Collections/List/6_Dictionary.py
@@ -0,0 +1,30 @@
+# key - only immutable types - string or int
+
+POINTS = {"x": 1, "y": 2}
+POINTS_FUNC = dict(x=1, y=2)
+
+print(POINTS)
+print(POINTS_FUNC)
+print(POINTS["x"])
+POINTS_FUNC["l"] = 3
+print(POINTS_FUNC["l"])
+
+if "l" in POINTS_FUNC:
+    print("L exist in dic:", POINTS_FUNC["l"])
+
+# Return None if not found or default value can be specified
+DEFAULT_VALUE = 0
+FIND_ELEMENT = POINTS_FUNC.get("S", DEFAULT_VALUE)
+print(FIND_ELEMENT)
+
+# Remove
+print(POINTS_FUNC)
+del POINTS_FUNC["l"]
+print(POINTS_FUNC)
+
+# Withour items() iterates by key
+for record in POINTS.items():
+    print(record)
+
+for key, value in POINTS.items():
+    print(f"KEY: {key} VALUE: {value}")
