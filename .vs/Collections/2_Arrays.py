6effd8bb37c537aebcd6ee8a993ae81156d8b150
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-01-30 21:39:13 +0100

    set

--- .vs/Collections/2_Arrays.py
+++ .vs/Collections/2_Arrays.py
@@ -1,6 +1,13 @@
 from array import array
 
+# Arrays are readonly
+# i - is type code -> python 3 typecode -> google
 numbers = array("i", [1, 2, 3])
+
+numbers.pop()
+numbers.append(0)
+numbers.insert(0, -1)
+numbers.remove(1)
 
 for number in numbers:
     print(number)
