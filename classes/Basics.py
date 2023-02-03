135a32f78c88c61ce05ba2b91d9bce6b2c12183d
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-02-03 21:21:32 +0100

    constructor

--- classes/Basics.py
+++ classes/Basics.py
@@ -1,8 +1,17 @@
 class Point:
+# __init__ - constructor. self - will always have refference in memory to new created instance
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+#by default method should have at least 1 parameter. Self is good as example
     def draw(self):
-        print("draw")
+        print(f"Points (X: {self.x} Y: {self.y})")
 
 
-point = Point()
+point = Point(1, 2)
+print(point.x)
+print(point.y)
+point.draw()
 print(type(point))
 print(isinstance(point, int))
