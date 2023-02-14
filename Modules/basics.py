c2022e0bd0f38da3046e26cd9f614e8b39e0c892
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-02-14 14:29:55 +0100

    import

--- Modules/basics.py
+++ Modules/basics.py
@@ -1,8 +1,11 @@
 # Modules - is file that containce refference to another file to build this module
 
-# Import can be done: from Example.sales import calc_shipping, calc_tax or: import example.sales
+# Import can be done:
+# Abosult import: from example.shopping import sales - the use sales
+# Relative import: from ..Modules import basics
+# Another used: from Example.sales import calc_shipping, calc_tax or: import example.sales
 
-from example.sales import calc_shipping
-from example import sales
+from example.shopping.sales import calc_shipping
+from example.shopping import sales
 
 sales.calc_shipping()
