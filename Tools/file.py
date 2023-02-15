e08bf42f1145fc8b1b4fde0a51e2d625b4181241
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-02-15 19:33:29 +0100

    zip

--- Tools/file.py
+++ Tools/file.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from time import ctime
 import shutil
 
-path = Path("Tools/testFile.txt")
+path = Path("Tools/testFiles/testFile.txt")
 
 # Check if exists
 print(path.exists())
@@ -29,6 +29,6 @@ path.write_text("extra text")
 with open(path, "r") as file:
     print(file.read())
 
-fileCopy = Path("Tools/Copy.txt")
+fileCopy = Path("Tools/testFiles/Copy.txt")
 shutil.copy(path, fileCopy)
 print(f"file copy text: {fileCopy.read_text()}")
