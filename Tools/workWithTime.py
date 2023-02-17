76792e0a8483eff5297191c928e3c04006bd455e
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-02-17 14:07:47 +0100

    TimeStamp

--- Tools/workWithTime.py
+++ Tools/workWithTime.py
@@ -1,4 +1,6 @@
 import time
+from datetime import datetime
+import time
 
 # returns current date time as time stamp
 # print(time.time())
@@ -14,3 +16,25 @@ send_email()
 END_TIME = time.time()
 DURATION = END_TIME - START_TIME
 print(DURATION)
+
+
+# Date Time
+CUSTOM_DATETIME = datetime(2018, 1, 1)
+CURRENT_DATETIME = datetime.now()
+
+# Format to specific format
+TIME_TO_FORMAT = "2018/01/01"
+datetime.strptime(TIME_TO_FORMAT, "%Y/%m/%d")
+print(TIME_TO_FORMAT)
+
+# -- Convert string to DateTime obj --
+
+# Convert from TimeStamp
+DATETIME_OBJ = datetime.fromtimestamp(time.time())
+print(DATETIME_OBJ)
+print(f"formatted year: {DATETIME_OBJ.year}")
+print(f"formatted month: {DATETIME_OBJ.month}")
+
+# Convert DateTime to string
+DATETIME_STRING = DATETIME_OBJ.strftime("%Y/%m")
+print(DATETIME_STRING)
