d45e0760a3b4ef5afe670022037f9a20e22e6292
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-02-20 13:59:57 +0100

    email template

--- Tools/automation_testing/sendingEmails.py
+++ Tools/automation_testing/sendingEmails.py
@@ -1,18 +1,28 @@
 # mime - multipurpose internet mail extensions
+# from string import Template - to replce params in HTML
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
-from pathlib
+from pathlib import Path
+from string import Template
 import smtplib
+
+EMAIL = "john@gmail.com"
+PASSWORD = "PASSWORD"
+
+TEMPLATE = Template(Path("emailTemplate.html").read_text())
+TEMPLATE.substitute()
 
 message = MIMEMultipart()
 message["from"] = "OHA"
-message["to"] = "olegenikus@gmail.com"
+message["to"] = EMAIL
 message["subject"] = "Test from python"
+
+body = TEMPLATE.substitute({"name": "John"})
 # set second parametr to 'html' to send html code
-message.attach(MIMEText("Body"))
-message.attach(MIMEImage(Pat))
+message.attach(MIMEText(body, "html"))
+message.attach(MIMEImage(Path("test_picture.png")))
 
 with smtplib.SMTP(host="smpt.gmail.com", port=587) as smpt:
     # Hello message to smpt server. It always should must start from this
@@ -20,6 +30,6 @@ with smtplib.SMTP(host="smpt.gmail.com", port=587) as smpt:
     # to encrypt everything
     smpt.starttls()
     # test user
-    smpt.login("olegenikus@gmail.com", "password")
+    smpt.login(EMAIL, PASSWORD)
     smpt.send_message(message)
     print("Sent...")
