4382ae3743da2bd9ae2143e46b5b1fb0634229a4
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-02-21 18:29:18 +0100

    v env

--- Tools/pip_tools/general.py
+++ Tools/pip_tools/general.py
@@ -1,9 +1,10 @@
 import subprocess
 import requests
 
+# Install and read all available packages
 subprocess.run("pip3 install requests==2.28.2")
 subprocess.run("pip3 list")
 
-
+# Usage installed package
 response = requests.get("https://www.google.com/")
 print(response)
