128f3faad0cc4794f50e5e938caa3f042b4a55e5
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-02-23 21:00:52 +0100

    seleium update

--- automation-testing/basicsS.py
+++ automation-testing/basicsS.py
@@ -1,17 +1,21 @@
 # pipenv install selenium
-# pipe install selenium
+# pip install selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+URL = "https://github.com/"
+SIGNUP_BUTTON = "//button[contains(@class, 'signup')]"
+ENTEREMAIL_LABEL = "//div[@id='email-container']//label[@class='text-mono signup-text-prompt']"
+
 BROWSER = webdriver.Chrome()
-BROWSER.get("https://github.com/")
+BROWSER.get(URL)
 BROWSER.find_element(
-    By.XPATH, "//button[contains(@class, 'signup')]").click()
+    By.XPATH, SIGNUP_BUTTON).click()
 
 assert "Welcome to GitHub!" in BROWSER.page_source
 
 LABEL = BROWSER.find_element(
-    By.XPATH, "//div[@id='email-container']//label[@class='text-mono signup-text-prompt']")
+    By.XPATH, ENTEREMAIL_LABEL)
 assert "Enter your email" in LABEL.text
 
 BROWSER.quit()
